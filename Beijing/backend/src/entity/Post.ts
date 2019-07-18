40ee4c1b5e492bd6344d07afb676cdcec11dc18c
lizunlong <lizunlong@gmail.com>
2019-07-18 16:46:17 +0800

    use uuid as id

--- Beijing/backend/src/entity/Post.ts
+++ Beijing/backend/src/entity/Post.ts
@@ -2,8 +2,8 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
 
 @Entity()
 export class Post {
-  @PrimaryGeneratedColumn()
-  id: number
+  @PrimaryGeneratedColumn('uuid')
+  id: string
 
   @Column()
   title: string
