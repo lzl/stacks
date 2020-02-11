0e1792194aaaae24b3eaff8a95ad23aea957e194
lizunlong <lizunlong@gmail.com>
2020-02-11 15:08:59 +0800

    use xstate

--- nextjs-xstate/pages/index.tsx
+++ nextjs-xstate/pages/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { NextPage } from 'next'
+import { useMachine } from '@xstate/react'
 
 import Layout from '../components/Layout'
+import Toggle from '../components/Toggle'
+import { toggleMachine } from '../machines/toggleMachine'
 
 const IndexPage: NextPage = () => {
+  const [toggleCurrent, toggleSend] = useMachine(toggleMachine)
+
   return (
     <Layout title="Next.js + XState">
-      <h1>LZL</h1>
+      <Toggle
+        onToggle={() => toggleSend('TOGGLE')}
+        active={toggleCurrent.matches('active')}
+      />
     </Layout>
   )
 }
